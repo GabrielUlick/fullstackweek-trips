065d2668b8f0b358e0a2b161fdeef79d2be19a21
Gabriel Ulick <gabrieludb@hotmail.com>
2023-07-06 23:21:40 -0300

    chore: add domains to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
